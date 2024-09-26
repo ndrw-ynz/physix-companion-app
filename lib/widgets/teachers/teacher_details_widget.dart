7298556321b6a74b581f81850df3ae555e8106ac
ndrw-ynz <andrewyanza4@gmail.com>
2024-09-26 17:46:21 +0800

    feat: add necessary teacher data on widget to allow editing teacher data with form

--- lib/widgets/teachers/teacher_details_widget.dart
+++ lib/widgets/teachers/teacher_details_widget.dart
@@ -1,22 +1,28 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:physix_companion_app/utils.dart';
 
 class TeacherDetailsWidget extends StatelessWidget {
   const TeacherDetailsWidget(
       {super.key,
       required this.itemNumber,
-      required this.name,
+      required this.uid,
+      required this.lastName,
+      required this.firstName,
       required this.email,
       required this.username,
       required this.password,
-      required this.yearRegistered});
+      required this.dateRegistered});
 
   final int itemNumber;
-  final String name;
+  final String uid;
+  final String lastName;
+  final String firstName;
   final String email;
   final String username;
   final String password;
-  final int yearRegistered;
+  final Timestamp dateRegistered;
 
   @override
   Widget build(BuildContext context) {
@@ -41,7 +47,7 @@ class TeacherDetailsWidget extends StatelessWidget {
                           style: const TextStyle(
                               fontSize: 22.0, fontWeight: FontWeight.bold)),
                       const SizedBox(width: 10), // Space between texts
-                      Text("$name",
+                      Text("$lastName, $firstName",
                           style: const TextStyle(
                               fontSize: 18.0,
                               fontStyle: FontStyle.italic,
@@ -53,7 +59,13 @@ class TeacherDetailsWidget extends StatelessWidget {
                     children: <Widget>[
                       IconButton(
                         onPressed: () =>
-                            context.push("/admin_home/teachers/edit"),
+                            context.push("/admin_home/teachers/edit", extra: {
+                          "uid": uid,
+                          "firstName": firstName,
+                          "lastName": lastName,
+                          "email": email,
+                          "dateRegistered": dateRegistered,
+                        }),
                         icon: const Icon(Icons.edit),
                         style: ElevatedButton.styleFrom(
                           padding: const EdgeInsets.symmetric(horizontal: 10),
@@ -109,10 +121,11 @@ class TeacherDetailsWidget extends StatelessWidget {
                       ]),
                       const SizedBox(width: 10),
                       Row(children: <Widget>[
-                        const Text("Year Registered:",
+                        const Text("Date Registered:",
                             style: TextStyle(fontWeight: FontWeight.bold)),
                         const SizedBox(width: 10),
-                        Text("$yearRegistered"),
+                        Text(formatTimestamp(dateRegistered)),
+                        //Text(yearRegistered.toDate().year.toString()),
                       ])
                     ],
                   ))
